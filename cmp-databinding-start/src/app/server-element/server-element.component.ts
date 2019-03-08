a8d3e1edddb14a7bdefda110ce4259fba31c444d
sspinit88 <sspinit88@gmail.com>
2019-03-08 09:50:10 +0300

    change tab when clicked

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ContentChild, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, AfterViewInit {
 
   @Input('srvElement') element: {
     type: string;
@@ -13,10 +13,20 @@ export class ServerElementComponent implements OnInit {
     content: string;
   };
 
+  // данные из ViewChild будут доступны после загрузки ngAfterViewInit()
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
+
   constructor() {
   }
 
   ngOnInit() {
+
   }
+
+  ngAfterViewInit() {
+    console.log(this.header.nativeElement.textContent);
+  }
+
 
 }
